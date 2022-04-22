cc79e0973acabb87fc5ebef965076559c91237e3
Valery Kamdem <valery.kamdem@celes-systems.com>
2022-04-22 02:40:29 +0200

    Export Funtion for Excel

---
+++ FileDataToExcel/Models/Datatxt.cs
@@ -0,0 +1,56 @@
+﻿namespace FileDataToExcel.Models
+{
+    public class Datatxt
+    {
+        public string? ID { get; set; }
+        public string? UNIT { get; set; }
+        public string? PTDESC { get; set; }
+        public string? KEYWORD { get; set; }
+        public string? PRIMMOD { get; set; }
+        public string? ASSOCDSP { get; set; }
+        public string? CDETAI { get; set; }
+        public string? HWYNUM { get; set; }
+        public string? PNTBOXTY { get; set; }
+        public string? BOXNUM { get; set; }
+        public string? PTDISCL { get; set; }
+        public string? SLOTNUM { get; set; }
+        public string? INPTSSLT { get; set; }
+        public string? LOADDEST { get; set; }
+        public string? EUDESC { get; set; }
+        public string? PVFORMAT { get; set; }
+        public string? PVEULO { get; set; }
+        public string? PVEUHI { get; set; }
+        public string? PVDSPLO { get; set; }
+        public string? PVDSPHI { get; set; }
+        public string? OVERVAL { get; set; }
+        public string? PVRNGOP { get; set; }
+        public string? PVCLAMP { get; set; }
+        public string? CALIBOFF { get; set; }
+        public string? AUXUNIT { get; set; }
+        public string? ALFMT { get; set; }
+        public string? ALDLOPT { get; set; }
+        public string? DLYTIME { get; set; }
+        public string? REALARM { get; set; }
+        public string? PVLOTP { get; set; }
+        public string? PVHITP { get; set; }
+        public string? PVALDB { get; set; }
+        public string? CNFERRPR { get; set; }
+        public string? DEVHIPR { get; set; }
+        public string? DEVLOPR { get; set; }
+        public string? PVHIPR { get; set; }
+        public string? PVLOPR { get; set; }
+        public string? UNREASPR { get; set; }
+        public string? CRITSCAN { get; set; }
+        public string? CCRANK { get; set; }
+        public string? EIPPCODE { get; set; }
+        public string? EIPEVENT { get; set; }
+        public string? EIPENB { get; set; }
+        public string? ALENBST { get; set; }
+        public string? PVTV { get; set; }
+        public string? PNTBOXIN { get; set; }
+        public string? PNTPCTY { get; set; }
+        public string? PCADDRI1 { get; set; }
+        public string? SPECIFI1 { get; set; }
+        public string? RNGCODE1 { get; set; }
+    }
+}
